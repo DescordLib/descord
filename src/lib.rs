11b8dde79d644866ff53e675b3a425ce71d68c63
Pranjal-Patel <pranjalpatel9838@gmail.com>
2024-03-16 19:18:21 +0530

    added prelude module

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,20 @@
+#![allow(unused)]
+
 mod client;
 mod consts;
 mod handlers;
+mod models;
 mod ws;
 
 pub use client::Client;
 pub use consts::intents;
 pub use handlers::EventHandler;
 pub use ws::payload::Payload;
+
+pub mod prelude {
+    pub use super::{
+        intents::GatewayIntent,
+        models::{guild::Guild, message_response::MessageData, ready_response::*, user::User, message_response::CreateMessageData},
+        Client, EventHandler, Payload,
+    };
+}
