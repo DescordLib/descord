253c2f0ece913457c233077fc7836d63538051e6
Pranjal-Patel <pranjalpatel9838@gmail.com>
2024-03-18 19:52:36 +0530

    added message edit function

---
+++ src/models/message_edit.rs
@@ -0,0 +1,29 @@
+use nanoserde::{DeJson, SerJson};
+
+use super::embed::Embed;
+
+#[derive(DeJson, SerJson, Default)]
+pub struct MessageEditData {
+    pub content: Option<String>,
+    pub embeds: Option<Vec<Embed>>,
+    pub flags: Option<usize>,
+    // TODO: allowed_mentions, attachments, files, components
+}
+
+impl From<String> for MessageEditData {
+    fn from(value: String) -> Self {
+        Self {
+            content: Some(value),
+            ..Default::default()
+        }
+    }
+}
+
+impl From<&str> for MessageEditData {
+    fn from(value: &str) -> Self {
+        Self {
+            content: Some(value.to_owned()),
+            ..Default::default()
+        }
+    }
+}
