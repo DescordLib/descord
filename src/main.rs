bb042890c225c2028bc487b554f892d496eb76bf
Pranjal-Patel <pranjalpatel9838@gmail.com>
2024-03-18 11:51:41 +0530

    minor changes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+const IMAGE: &str = "https://images-ext-1.discordapp.net/external/O6IOVg-aIK9hoSwd1UtEQmgrAmxlpyzYjS2rW6shLgk/%3Fsize%3D4096/https/cdn.discordapp.com/avatars/815189874478546954/6a7a25edccdcabbe9f29558a0b6a7f00.png?format=webp&quality=lossless";
+
 use descord::prelude::*;
 
 #[tokio::main]
@@ -8,7 +10,8 @@ async fn main() {
     let client = Client::new(
         &std::env::var("DISCORD_TOKEN").unwrap(),
         GatewayIntent::MessageContent | GatewayIntent::GuildMessages,
-    ).await;
+    )
+    .await;
 
     client.login(Handler).await;
 }
@@ -17,17 +20,37 @@ struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn ready(&self, _: &Context, ready_data: ReadyData) {
+    async fn ready(&self, _ctx: &Context, ready_data: ReadyData) {
         println!(
             "Logged in as: {}#{}",
             ready_data.user.username, ready_data.user.discriminator
         );
     }
 
-    async fn message_create(&self, ctx: &Context, message_data: MessageData) {
-        if message_data.content == ".ping" {
-            ctx.reply(&message_data, "Pong (reply)").await;
-            ctx.send(&message_data.channel_id, "Pong").await;
+    async fn message_create(&self, ctx: &Context, data: MessageData) {
+        if data.author.bot {
+            return;
+        }
+
+        if data.content == "test" && data.author.user_id == "815189874478546954" {
+            let embed = EmbedBuilder::new()
+                .title("An Embed")
+                .description("Embed description")
+                .color(Color::Teal)
+                .field("field 1", "value 1", false)
+                .field("field 2", "value 2", false)
+                .field("field 3", "value 3", false)
+                .footer("Footer text", Some(IMAGE.to_owned()), None)
+                .build();
+
+            ctx.reply(
+                &data,
+                CreateMessageData {
+                    embeds: vec![embed],
+                    ..Default::default()
+                },
+            )
+            .await;
         }
     }
 }
--- src/ws/websocket_manager.rs
+++ src/ws/websocket_manager.rs
@@ -2,7 +2,7 @@ use log::*;
 use nanoserde::DeJson;
 
 use std::str::FromStr;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 use std::time::Duration;
 use std::{clone, thread};
 
@@ -12,6 +12,7 @@ use futures_util::{future, pin_mut, SinkExt, StreamExt};
 use tokio::io::{AsyncRead, AsyncWrite};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
+use tokio::sync::Mutex;
 
 use tokio_tungstenite::tungstenite::{Message, Result};
 use tokio_tungstenite::MaybeTlsStream;
@@ -26,13 +27,13 @@ use crate::handlers::EventHandler;
 use crate::ws::payload::Payload;
 use crate::{models::*, Client};
 
+type SocketWrite = Arc<Mutex<SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>>>;
+type SocketRead = Arc<Mutex<SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>>>;
+
 pub struct WsManager {
     ctx: Arc<Context>,
     token: String,
-    socket: (
-        Arc<Mutex<SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>>>,
-        Arc<Mutex<SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>>>,
-    ),
+    socket: (SocketWrite, SocketRead),
 }
 
 impl WsManager {
@@ -54,19 +55,19 @@ impl WsManager {
         intents: u32,
         event_handler: Arc<impl EventHandler + std::marker::Sync + 'static>,
     ) -> Result<()> {
-        if let Some(Ok(Message::Text(body))) = self.socket.1.lock().unwrap().next().await {
+        if let Some(Ok(Message::Text(body))) = self.socket.1.lock().await.next().await {
             let Some(payload) = Payload::parse(&body) else {
                 panic!("Failed to parse json, body: {body}");
             };
 
             match payload.operation_code {
                 OpCode::Hello => {
-                    info!("starting heartheat");
                     let time_ms = payload.data["heartbeat_interval"].as_u64().unwrap();
                     let writer = Arc::clone(&self.socket.0);
+                    let reader = Arc::clone(&self.socket.1);
 
                     tokio::spawn(async move {
-                        Self::heartbeat_start(Duration::from_millis(time_ms), writer);
+                        Self::heartbeat_start(Duration::from_millis(time_ms), writer, reader).await;
                     });
 
                     info!("performing handshake");
@@ -77,7 +78,7 @@ impl WsManager {
             }
         }
 
-        while let Some(Ok(Message::Text(body))) = self.socket.1.lock().unwrap().next().await {
+        while let Some(Ok(Message::Text(body))) = self.socket.1.lock().await.next().await {
             let Some(payload) = Payload::parse(&body) else {
                 error!("Failed to parse json");
                 continue;
@@ -89,12 +90,13 @@ impl WsManager {
                     let ctx = Arc::clone(&self.ctx);
 
                     info!(
-                        "received {} event",
+                        "received {} event\npayload:{}",
                         payload
                             .type_name
                             .as_ref()
                             .map(|i| i.as_str())
-                            .unwrap_or("Unknown")
+                            .unwrap_or("Unknown"),
+                        json::parse(&payload.raw_json).unwrap().pretty(4)
                     );
 
                     tokio::spawn(async move {
@@ -106,6 +108,8 @@ impl WsManager {
             }
         }
 
+        info!("Exiting...");
+
         Ok(())
     }
 
@@ -115,7 +119,6 @@ impl WsManager {
         ctx: Arc<Context>,
     ) {
         let event = Event::from_str(payload.type_name.as_ref().unwrap().as_str()).unwrap();
-
         match event {
             Event::Ready => {
                 let ready_data = ready_response::ReadyResponse::deserialize_json(&payload.raw_json)
@@ -147,14 +150,16 @@ impl WsManager {
 
     async fn heartbeat_start(
         heartbeat_interval: Duration,
-        writer: Arc<Mutex<SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>>>,
+        writer: SocketWrite,
+        reader: SocketRead,
     ) {
         let mut last_sequence: usize = 0;
         loop {
             let message = Message::Text(json::stringify(payloads::heartbeat(last_sequence)));
+            info!("sending heartbeat");
             writer
                 .lock()
-                .unwrap()
+                .await
                 .send(message)
                 .await
                 .expect("Failed to send heartbeat");
@@ -213,6 +218,6 @@ impl WsManager {
     }
 
     async fn send_text(&self, msg: String) -> Result<()> {
-        self.socket.0.lock().unwrap().send(Message::Text(msg)).await
+        self.socket.0.lock().await.send(Message::Text(msg)).await
     }
 }
