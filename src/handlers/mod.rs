bebb94ca4eb72a61690bd852f97ba8f37fd143f2
Pranjal-Patel <pranjalpatel9838@gmail.com>
2024-03-18 18:14:29 +0530

    update function signature

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,6 +1,5 @@
 use std::future::Future;
 
-use crate::client::Context;
 use crate::ws::payload::Payload;
 use crate::Client;
 
@@ -13,14 +12,14 @@ use async_trait::async_trait;
 #[async_trait]
 pub trait EventHandler: std::marker::Send + std::marker::Sync {
     /// Called when the client becomes ready to start working.
-    async fn ready(&self, ctx: &Context, ready_data: ReadyData) {}
+    async fn ready(&self, ready_data: ReadyData) {}
 
     /// Called whenever a message is sent.
-    async fn message_create(&self, ctx: &Context, data: MessageData) {}
+    async fn message_create(&self, data: MessageData) {}
 
     /// Called whenever a message is update i.e. content change.
-    async fn message_update(&self, ctx: &Context, data: MessageData) {}
+    async fn message_update(&self, data: MessageData) {}
 
     /// Called whenever a message is deleted.
-    async fn message_delete(&self, ctx: &Context, data: DeletedMessageData) {}
+    async fn message_delete(&self, data: DeletedMessageData) {}
 }
