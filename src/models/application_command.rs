d105112c4dadf72259aa44fa7c362138cb45edbf
Pranjal-Patel <pranjalpatel9838@gmail.com>
2024-03-24 12:22:43 +0530

    create app command struct

---
+++ src/models/application_command.rs
@@ -0,0 +1,48 @@
+use std::collections::HashMap;
+
+use nanoserde::{DeJson, SerJson};
+
+#[derive(Debug, Clone, DeJson, SerJson)]
+pub struct ApplicationCommand {
+    pub id: String,
+    #[nserde(rename = "type")]
+    pub type_: Option<u32>,
+    pub application_id: String,
+    pub guild_id: Option<String>,
+    pub name: String,
+    // name_localizations: Option<>,
+    pub description: String,
+    // description_localizations: Option<>,
+    pub options: Vec<ApplicationCommandOption>,
+    pub default_member_permissions: Option<String>,
+    pub nsfw: Option<bool>,
+    pub integration_types: Vec<u32>,
+    pub contexts: Option<Vec<u32>>,
+    pub version: String,
+}
+
+#[derive(DeJson, SerJson, Clone, Debug)]
+pub struct ApplicationCommandOption {
+    #[nserde(rename = "type")]
+    pub type_: u32,
+    pub name: String,
+    pub name_localizations: Option<HashMap<String, String>>,
+    pub description: String,
+    pub description_localizations: Option<HashMap<String, String>>,
+    pub required: Option<bool>,
+    pub choices: Option<Vec<ApplicationCommandOptionChoice>>,
+    pub options: Option<Vec<ApplicationCommandOption>>,
+    pub channel_types: Option<Vec<u32>>,
+    pub min_value: Option<i32>,
+    pub max_value: Option<i32>,
+    pub min_length: Option<i32>,
+    pub max_length: Option<i32>,
+    pub autocomplete: Option<bool>,
+}
+
+#[derive(DeJson, SerJson, Clone, Debug)]
+pub struct ApplicationCommandOptionChoice {
+    pub name: String,
+    pub name_localizations: Option<HashMap<String, String>>,
+    pub value: String,
+}
--- src/models/interaction.rs
+++ src/models/interaction.rs
@@ -137,32 +137,6 @@ pub struct AppCommandInteractionData {
 }
 
 #[derive(DeJson, SerJson, Clone, Debug)]
-pub struct ApplicationCommandOption {
-    #[nserde(rename = "type")]
-    pub type_: u32,
-    pub name: String,
-    pub name_localizations: Option<HashMap<String, String>>,
-    pub description: String,
-    pub description_localizations: Option<HashMap<String, String>>,
-    pub required: Option<bool>,
-    pub choices: Option<Vec<ApplicationCommandOptionChoice>>,
-    pub options: Option<Vec<ApplicationCommandOption>>,
-    pub channel_types: Option<Vec<u32>>,
-    pub min_value: Option<i32>,
-    pub max_value: Option<i32>,
-    pub min_length: Option<i32>,
-    pub max_length: Option<i32>,
-    pub autocomplete: Option<bool>,
-}
-
-#[derive(DeJson, SerJson, Clone, Debug)]
-pub struct ApplicationCommandOptionChoice {
-    pub name: String,
-    pub name_localizations: Option<HashMap<String, String>>,
-    pub value: String,
-}
-
-#[derive(DeJson, SerJson, Clone, Debug)]
 pub struct InteractionResponse {
     #[nserde(rename = "type")]
     pub type_: u32,
